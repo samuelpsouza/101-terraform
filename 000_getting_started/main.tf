6135c630ddf078a8af5cc87a1c62fadb74db9088
Samuel Souza <samuelsouza.contact@gmail.com>
2023-12-21 13:43:04 -0300

    Getting started with Terraform

---
+++ 000_getting_started/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "app_server"
+  }
+
+}
